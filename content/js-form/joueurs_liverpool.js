ebecc368e8220c365fbbfed46d2527be30546529
Joretapau <lebars.brieuc@gmail.com>
2019-04-22 17:38:05 +0200

    correction fichier liverpool js

--- content/js-form/joueurs_liverpool.js
+++ content/js-form/joueurs_liverpool.js
@@ -1,6 +1,6 @@
 var gardien = [ "Alisson Becker", "Caoimhin Kelleher", "Simon Mignolet"];
 
-var defenseurs = ["Joe Gomez", "Ki-Jana Hoever", "Dejan Lovren", "Conor Masterson", "Joël Matip", "Alberto Moreno", , "Nathaniel Phillips", "Andrew Robertson", "Virgil Van Dijk"];
+var defenseurs = ["Joe Gomez", "Ki-Jana Hoever", "Dejan Lovren", "Conor Masterson", "Joël Matip", "Alberto Moreno", "Nathaniel Phillips", "Andrew Robertson", "Virgil Van Dijk"];
 
 var milieux = ["Trent Alexander-Arnold", "Rafael Camacho", "Isaac Christie-Davis", "Fabinho", "Roberto Firmino", "Jordan Henderson", "Curtis Jones", "Naby Keita", "Adam Lallana", "Sadio Mané", "James Milner", "Alex Oxlade-Chamberlain", "Mohamed Salah", "Xherdan Shaqiri", "Georginio Wijnaldum"];
 
